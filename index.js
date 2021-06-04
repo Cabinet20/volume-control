e8f169348e07e2af461961eeadf83710efe863a0
Bren Gunning <github@cinnamonrage.com>
2021-06-04 23:21:28 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+DeviceOrientationEvent.requestPermission().then(res => {
+	alert(res);
+  console.log(res);
+  if (res === 'granted') {
+    window.addEventListener('deviceorientation', (e) => {
+	  document.getElementById('output').innerHTML = e;
+      console.log(e);
+    });
+  }
+});
